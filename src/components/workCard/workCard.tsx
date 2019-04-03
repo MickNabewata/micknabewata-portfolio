f92f7fc91a65eb8a4822d1caf89620f2653aa05e
unknown <watanabemi@ad.techvan.co.jp>
2019-04-03 13:30:24 +0900

    update

--- src/components/workCard/workCard.tsx
+++ src/components/workCard/workCard.tsx
@@ -2,17 +2,16 @@ import React from 'react';
 import styles from './workCardStyles';
 import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { Url } from 'url';
 
 /** プロパティ型定義 */
 interface Prop extends WithStyles<typeof styles> {
+  workInfo : Work
 }
 
 /** ステート型定義 */
@@ -20,17 +19,21 @@ type State = {
 };
 
 /** 開発実績型定義 */
-type Work = {
+export type Work = {
+  /** 画像URL */
+  ImageUrl? : string | '',
+  /** 件名 */
+  Name : string,
   /** 技術 */
   Skill : string[],
   /** 役割 */
-  Rolle : String[],
+  Role : String[],
   /** 人数 */
   Members : Number,
   /** URL */
-  URL : Url,
+  URL? : string,
   /** GitHub */
-  GitHub : Url,
+  GitHub? : string,
   /** 概要(1要素ずつ改行表示) */
   Overview : string[]
 }
@@ -48,40 +51,70 @@ class WorkCard extends React.Component<Prop, State> {
     };
   }
 
-  /** 開発実績を取得 */
-  getWorks() : Work[]
-  {
-    return [];
-  }
-
   /** レンダリング */
   render() {
     return (
       <Card className={this.props.classes.card}>
         <CardMedia
           className={this.props.classes.media}
-          image='http://pet-seikatsu.jp/images/2015/08/2d85d2a2063d1ed3b3271144167cdf85-large.jpg'
-          title='Image Title'
+          image={this.props.workInfo.ImageUrl}
+          title={this.props.workInfo.Name}
         />
         <CardContent>
           <Typography component='h2' variant='h5' gutterBottom >
-            ○×案件
+            {this.props.workInfo.Name}
           </Typography>
           <Paper elevation={0}>
             <Typography component="h3" variant="h6">
-              This is a sheet of paper.
+              技術
             </Typography>
             <Typography component="p">
-              Paper can be used to build surface or other elements for your application.
+              {this.props.workInfo.Skill}
             </Typography>
           </Paper>
           <Paper elevation={0}>
             <Typography component="h3" variant="h6">
-              This is a sheet of paper.
+              役割
             </Typography>
             <Typography component="p">
-              Paper can be used to build surface or other elements for your application.
+              {this.props.workInfo.Role}
             </Typography>
+          </Paper>
+          <Paper elevation={0}>
+            <Typography component="h3" variant="h6">
+              人数
+            </Typography>
+            <Typography component="p">
+              {this.props.workInfo.Members}
+            </Typography>
+          </Paper>
+          <Paper elevation={0}>
+            <Typography component="h3" variant="h6">
+              URL
+            </Typography>
+            <Typography component="p">
+              {this.props.workInfo.URL}
+            </Typography>
+          </Paper>
+          <Paper elevation={0}>
+            <Typography component="h3" variant="h6">
+              GitHub
+            </Typography>
+            <Typography component="p">
+              {this.props.workInfo.GitHub}
+            </Typography>
+          </Paper>
+          <Paper elevation={0}>
+            <Typography component="h3" variant="h6">
+              概要
+            </Typography>
+            {this.props.workInfo.Overview.map((val) => {
+              return (
+                <Typography component="p">
+                  {val}
+                </Typography>
+              )
+            })}
           </Paper>
         </CardContent>
         <CardActions>
--- src/components/works/works.tsx
+++ src/components/works/works.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import styles from './worksStyles';
 import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
-import WorkCard from '../workCard/workCard';
-import Typography from '@material-ui/core/Typography';
+import WorkCard, { Work } from '../workCard/workCard';
 
 /** プロパティ型定義 */
 interface Prop extends WithStyles<typeof styles> {
@@ -25,13 +24,70 @@ class Works extends React.Component<Prop, State> {
     };
   }
 
+  /** 開発実績を取得 */
+  getWorks() : Work[]
+  {
+    return [
+      {
+        ImageUrl : 'http://pet-seikatsu.jp/images/2015/08/2d85d2a2063d1ed3b3271144167cdf85-large.jpg',
+        Name : 'Sample1',
+        Skill : ['C#', '', 'ASP.NET', ''],
+        Role : ['メンバー', '', 'リーダー', ''],
+        Members : 1,
+        URL : 'http://google.co.jp',
+        GitHub : 'http://yahoo.co.jp',
+        Overview : ['行1','行2','','行3', '']
+      },
+      {
+        ImageUrl : 'http://pet-seikatsu.jp/images/2015/08/2d85d2a2063d1ed3b3271144167cdf85-large.jpg',
+        Name : 'Sample2',
+        Skill : ['C#'],
+        Role : ['メンバー'],
+        Members : 2,
+        URL : 'http://google.co.jp',
+        GitHub : 'http://yahoo.co.jp',
+        Overview : ['行1']
+      },
+      {
+        ImageUrl : '',
+        Name : '',
+        Skill : [''],
+        Role : [''],
+        Members : 3,
+        URL : '',
+        GitHub : '',
+        Overview : ['']
+      },
+      {
+        Skill : [''],
+        Name : '',
+        Role : [''],
+        Members : 4,
+        Overview : ['']
+      },
+      {
+        ImageUrl : undefined,
+        Name : '',
+        Skill : [],
+        Role : [],
+        URL : undefined,
+        GitHub : undefined,
+        Members : 5,
+        Overview : []
+      },
+    ];
+  }
+
   /** レンダリング */
   render() {
+
+    let works = this.getWorks();
+
     return (
         <React.Fragment>
-          <WorkCard />
-          <WorkCard />
-          <WorkCard />
+          {works.map((work) => {
+            return <WorkCard workInfo={work} />
+          })}
         </React.Fragment>
     );
   }
